6e8f49018e321d93dd44b4cfb5ff74a0289bd0b8
yychen <yychen@utstar.com>
2020-08-06 15:36:22 +0800

    feat: 用户设置单元测试

--- test/user/login.test.js
+++ test/user/login.test.js
@@ -52,11 +52,35 @@ test('登录，应该成功', async () => {
         userName,
         password
     })
-    console.log(res.body)
     expect(res.body.errno).toBe(0)
 
     // 获取 cookie
     COOKIE = res.headers['set-cookie'].join(';')
+})
+
+// 修改基本信息
+test('修改基本信息，应该成功', async () => {
+    const res = await server
+        .patch('/api/user/changeInfo')
+        .send({
+            nickName: '测试昵称',
+            city: '测试城市',
+            picture: '/test.png'
+        })
+        .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+})
+
+// 修改密码
+test('修改密码，应该成功', async () => {
+    const res = await server
+        .patch('/api/user/changePassword')
+        .send({
+            password,
+            newPassword: `p_${Date.now()}`
+        })
+        .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
 })
 
 // 删除
@@ -65,8 +89,17 @@ test('删除用户，应该成功', async () => {
     expect(res.body.errno).toBe(0)
 })
 
+// 推出登录
+test('退出登录，应该成功', async () => {
+    const res = await server
+        .post('/api/user/logout')
+        .set('cookie', COOKIE)
+    expect(res.body.errno).toBe(0)
+})
+
 // 再次查询用户，应该不存在
 test('删除之后，查询注册的用户名，应该不存在', async () => {
     const res = await server.post('/api/user/isExist').send({userName})
     expect(res.body.errno).not.toBe(0)
 })
+
