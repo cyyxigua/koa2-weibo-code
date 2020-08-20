554075cb569da923b3017fa2b2d2d2b98122da29
yychen <yychen@utstar.com>
2020-08-20 17:29:28 +0800

    test: 关注和取消关注单元测试

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -4,6 +4,11 @@
  */
 
 module.exports = {
-   USER_NAME: 'zhangsan',
-   COOKIE: 'weibo.sid=x7v5KvXdPa_hnc4-tIT9iYvDwyJ7PEXD; weibo.sid.sig=LSMp-sItS5zi2QEvzvfibSMnfaE'
+   Z_ID: 2,
+   Z_USER_NAME: 'zhangsan',
+   Z_COOKIE: 'weibo.sid=2T9GYQGZeQj3dgKcfSTN6b5glEISQXXO; weibo.sid.sig=SHN3fAvpoW5RmrAqRiPTSabzZ6Q',
+
+   L_ID: 1,
+   L_USER_NAME: 'lisi',
+   L_COOKIE: 'weibo.sid=F3uaMNSUm_mOsI4PupNoiAZZTfvD_nME; weibo.sid.sig=0z9Jb2VKDosrqfM6Lhd9BICgt98'
 }
