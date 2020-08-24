f6e98966845a7e5edc7ef0fe578d870ac3629d8c
yychen <yychen@utstar.com>
2020-08-24 10:32:25 +0800

    feat: 首页 数据模型

--- src/services/user.js
+++ src/services/user.js
@@ -5,6 +5,7 @@
 
 const { User } = require('../db/model/index')
 const {formatUser} = require('./_format')
+const { addFollower } = require('./user-relation')
 
 /**
  * 获取用户信息
@@ -50,7 +51,13 @@ async function createUser({userName, password, gender = 3, nickName}) {
     gender,
     nickName: nickName ? nickName : userName
   })
-  return result
+
+  const data = result.dataValues
+
+  // 自己关注自己（为了方便首页获取数据）
+  await addFollower(data.id, data.id)
+
+  return data
 }
 
 /**
