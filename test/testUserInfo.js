16dca143ee6492df3acea159b6074f1cd392d0c9
yychen <yychen@utstar.com>
2020-08-25 16:04:45 +0800

    test: getAtList

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -6,9 +6,9 @@
 module.exports = {
    Z_ID: 1,
    Z_USER_NAME: 'zhangsan',
-   Z_COOKIE: 'weibo.sid=hPF5uSx9b4UIKtfROcH3IA5UKFqKBTxo; weibo.sid.sig=c6s0P0mlYKWr5XJecnVwjUCR94k',
+   Z_COOKIE: 'weibo.sid=H-7F1riXun0Nc-KgzYxZLG_YxvNzA2xA; weibo.sid.sig=KQPDKzouf3pu5IPt4Wa5202Fq74',
 
    L_ID: 2,
    L_USER_NAME: 'lisi',
-   L_COOKIE: 'weibo.sid=29SB-BYTjO1vGBE37IcwayvB88Fs4fAO; weibo.sid.sig=276ibZxcwgkY6NMRxCHCiz9W6m8'
+   L_COOKIE: 'weibo.sid=YYoNiXnO8cQ9KdCTS58CRvENOnF7lBNy; weibo.sid.sig=4wg_Sv3pUutw8uECmhwk738wZS0'
 }
