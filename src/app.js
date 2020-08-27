83b5fbc79a15dce88e3a7905ed99d389ed7ac136
yychen <yychen@utstar.com>
2020-08-27 15:46:46 +0800

    feat: @me页面加载更多

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const { isProd } = require('./utils/env')
 const { SESSION_SECRET_KEY } = require('./conf/secretKeys')
 
 // 路由
+const atAPIRouter = require('./routes/api/blog-at')
 const squareAPIRouter = require('./routes/api/blog-square')
 const profileAPIRouter = require('./routes/api/blog-profile')
 const homeRouter = require('./routes/api/blog-home')
@@ -71,6 +72,7 @@ app.use(session({
 }))
 
 // routes
+app.use(atAPIRouter.routes(), atAPIRouter.allowedMethods())
 app.use(squareAPIRouter.routes(), squareAPIRouter.allowedMethods())
 app.use(profileAPIRouter.routes(), profileAPIRouter.allowedMethods())
 app.use(homeRouter.routes(), homeRouter.allowedMethods())
