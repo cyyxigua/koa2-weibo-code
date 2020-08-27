fa58c11f5b4b052cfabde0e88d975148d26586e4
yychen <yychen@utstar.com>
2020-08-27 15:36:51 +0800

    feat: @me 页面

--- src/controller/blog-at.js
+++ src/controller/blog-at.js
@@ -3,8 +3,9 @@
  * @author cyy
  */
 
-const { getAtRelationCount } = require('../services/at-relation')
+const { getAtRelationCount, getAtUserBlogList } = require('../services/at-relation')
 const { SuccessModel } = require('../model/ResModel')
+const { PAGE_SIZE } = require('../conf/constants')
 
 /**
  * 获取 @ 我的微博数量
@@ -17,6 +18,25 @@ async function getAtMeCount(userId) {
   })
 }
 
+/**
+ * 获取 @ 用户的微博列表
+ * @param {number} userId 用户 ID 
+ * @param {number} pageIndex 当前页
+ */
+async function getAtMeBlogList(userId, pageIndex = 0) {
+  const result = await getAtUserBlogList({userId, pageIndex, PAGE_SIZE})
+  const { count, blogList } = result
+
+  return new SuccessModel({
+    isEmpty: blogList.length === 0,
+    blogList,
+    pageSize: PAGE_SIZE,
+    pageIndex,
+    count
+  })
+}
+
 module.exports = {
-  getAtMeCount
+  getAtMeCount,
+  getAtMeBlogList
 }
--- src/services/at-relation.js
+++ src/services/at-relation.js
@@ -3,7 +3,8 @@
  * @author cyy
  */
 
-const { AtRelation } = require('../db/model/index')
+const { AtRelation, Blog, User } = require('../db/model/index')
+const { formatBlog, formatUser } = require('./_format')
 
 /**
  * 创建微博 @ 用户的关系
@@ -32,7 +33,49 @@ async function getAtRelationCount(userId) {
   return result.count
 }
 
+/**
+ * 获取 @ 用户的微博列表
+ * @param {*} userId 用户 id
+ * @param {*} pageIndex 当前页
+ * @param {*} pageSize 每页条数
+ */
+async function getAtUserBlogList({userId, pageIndex, pageSize = 10}) {
+  const result = await Blog.findAndCountAll({
+    limit: pageSize,
+    offset: pageSize * pageIndex,
+    order: [
+      ['id', 'desc']
+    ],
+    include: [
+      // @ 关系
+      {
+        model: AtRelation,
+        attributes: ['userId', 'blogId'],
+        where: { userId }
+      },
+      {
+        model: User,
+        attributes: ['userName', 'nickName', 'picture']
+      }
+    ]
+  })
+  
+  // 格式化
+  let blogList = result.rows.map(row => row.dataValues)
+  blogList = formatBlog(blogList)
+  blogList = blogList.map(blogItem => {
+    blogItem.user = formatUser(blogItem.user.dataValues)
+    return blogItem
+  })
+
+  return {
+    count: result.count,
+    blogList
+  }
+}
+
 module.exports = {
   createAtRelation,
-  getAtRelationCount
+  getAtRelationCount,
+  getAtUserBlogList
 }
